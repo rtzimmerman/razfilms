c97ed54cb5b143657ae2f9962e91ee4cd9b3d59a
Robert Zimmerman <zim2007@gmail.com>
2018-08-12 15:21:19 -0700

    Add express node server for hosting static files on Heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+
+app.use(express.static(__dirname+'/dist/my-app'));
+
+//Path Location Strategy
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+  });
+app.listen(process.env.PORT || 5001);
+
+console.log('Server running on port: ', process.env.PORT);
+
+
