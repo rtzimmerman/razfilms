20c813c3c3d06ee4f770b45fc62c7e85d964e3ac
Robert Zimmerman <zim2007@gmail.com>
2018-08-12 16:16:24 -0700

    removed base-href

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname+'/dist/my-app'));
 
 //Path Location Strategy
 app.get('/*', (req,res) => {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
   });
 app.listen(process.env.PORT || 5001);
 
