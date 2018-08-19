26b4ca58b476292e65baf56e862ea307208e6a86
Robert Zimmerman <zim2007@gmail.com>
2018-08-19 11:53:01 -0700

    Added films component; styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,12 @@ import { FooterComponent } from './footer/footer.component';
 import { SocialMediaComponent } from './social-media/social-media.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { FilmsComponent } from './films/films.component'
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'films', component: FilmsComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
 ];
@@ -24,7 +26,8 @@ const appRoutes: Routes = [
     FooterComponent,
     SocialMediaComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
