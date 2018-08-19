42169be01b7edbb9ca978b8a1f1150984605c0d7
Robert Zimmerman <zim2007@gmail.com>
2018-08-19 13:42:04 -0700

    fixes to slider menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  private menuItemsArray: any[] = [
+  public menuItemsArray: any[] = [
        {"title":"Home","link":"home"},
        {"title":"Films","link":"films"},
        {"title":"About","link":"about"},
        {"title":"Contact","link":"contact"},
     ];
-    
+      public onMenuClose(){
+        console.log("menu closed");
+      }
+      public onMenuOpen(){
+        console.log("menu Opened");
+      }
+      public onItemSelect(item:any){
+        window.location.assign(item.link);
+        console.log(item);
+      }
+      
   wow() {
     console.log('wow');
   };
